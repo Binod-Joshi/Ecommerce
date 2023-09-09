5fb0824cfed735a11334dc39b0b9c1cb1f3fddad
Binod-Joshi <111906023+Binod-Joshi@users.noreply.github.com>
2023-09-09 23:49:56 +0530

    update private

--- ecomfrontend/src/pages/forms/setting/Logout.js
+++ ecomfrontend/src/pages/forms/setting/Logout.js
@@ -3,25 +3,49 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const StyledLogout = styled.div`
+
 display:flex;
 align-items:center;
 justify-content:center;
-`
+flex-direction:column;
+background-color:gray;
+
+
+h1 {
+    width:280px;
+}
+
+.cancelAndOk {
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    gap:98%;
+
+    button {
+      margin-right:10px;
+
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+  }
+`;
 const Logout = () => {
     const navigate = useNavigate();
 
     const cancelHandler = () => {
-
+        navigate('/usehome')
     }
 
     const okHandler = () => {
-        
+    
     }
 
     return(
         <>
         <StyledLogout>
         <h1>Are you sure you want to Logout.</h1>
+        <hr />
         <div className="cancelAndOk">
             <button onClick={cancelHandler}>Cancel</button>
             <button onClick={okHandler}>Ok</button>
